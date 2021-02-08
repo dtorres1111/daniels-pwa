cf70fc17c07e412cdd62e0904c31541dcf0f19a3
dtorres1111 <dtcatering@outlook.dk>
2021-02-08 14:48:43 +0100

    add

--- public/OneSignalSDKUpdaterWorker.js
+++ public/OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
-importScripts('https://602134058bacca7ac2330149--quizzical-saha-438c72.netlify.app/service-worker.js')
+importScripts('https://quizzical-saha-438c72.netlify.app/service-worker.js')
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
