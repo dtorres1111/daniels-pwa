eacd98011c91a109c1c8237fadd7bdf972b92395
dtorres1111 <dtcatering@outlook.dk>
2021-02-08 13:48:13 +0100

    update package log

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+//bla
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
